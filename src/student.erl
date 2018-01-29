c4f7157c45fd3e65bd31ab0997f1f815c84eb3b4
Milos Vasic <Milos.Vasic@voiceworks.com>
2018-01-29 15:32:03 +0100

    Student module.

---
+++ src/student.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author milosvasic
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29. Jan 2018 3:24 PM
+%%%-------------------------------------------------------------------
+-module(student).
+-author("milosvasic").
+
+%% API
+-export([init/1]).
+
+init(name) -> name.
